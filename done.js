7e2a48886b97ed34d59a92f089bc367a9bfb6581
andrewliebchen <andrewliebchen@gmail.com>
2014-09-19 23:47:21 -0700

    Trying to group by days

--- done.js
+++ done.js
@@ -1,5 +1,6 @@
-if (Meteor.isClient) {
+Tasks = new Meteor.Collection('tasks');
 
+if (Meteor.isClient) {
   Template.question.currentTime = function(){
     return moment().format('h:mm a');
   };
@@ -7,6 +8,18 @@ if (Meteor.isClient) {
   Template.question.currentDay = function(){
     return moment().format('dddd');
   };
+
+  Template.history.post = function(){
+    return Tasks.find({});
+  }
+
+  // Template.history.day = function(){
+  //   var tasks = Tasks.find().fetch();
+  //   var groupedDates = _.groupBy(_.pluck(tasks, 'createdAt'));
+  //   _.each(_.values(groupedDates), function(day) {
+  //     console.log({Date: day[0], Total: day.length});
+  //   });
+  // }
 
   Template.question.events({
 
@@ -21,4 +34,24 @@ if (Meteor.isServer) {
   Meteor.methods({
 
   });
+
+  // Seed
+  if(Tasks.find().count() === 0) {
+    Tasks.insert({
+      name:      'Installed a fresh instance of Meteor',
+      createdAt: moment().format('YYYYMMDD')
+    });
+    Tasks.insert({
+      name:      'Created a new branch on the Done repo',
+      createdAt: moment().format('YYYYMMDD')
+    });
+    Tasks.insert({
+      name:      'Installed the MomentJS package',
+      createdAt: moment().format('YYYYMMDD')
+    });
+    Tasks.insert({
+      name:      'Created some a basic template',
+      createdAt: moment().format('YYYYMMDD')
+    });
+  }
 }
