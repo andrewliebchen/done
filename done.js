9de796241b54d09a178b5aec7f60db19ebde89bb
andrewliebchen <andrewliebchen@gmail.com>
2014-09-19 22:06:40 -0700

    Use moment to get current time

--- done.js
+++ done.js
@@ -1,23 +1,24 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+  Template.question.currentTime = function(){
+    return moment().format('h:mm a');
+  };
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
+  Template.question.currentDay = function(){
+    return moment().format('dddd');
+  };
+
+  Template.question.events({
+
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
+  });
+
+  Meteor.methods({
+
   });
 }
